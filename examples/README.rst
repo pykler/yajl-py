4cc3ece37fe6f8b3155534ae7e56c1dab739b90e
Hatem Nassrat <hnassrat@gmail.com>
2010-05-25 02:38:00 -0300

    Implemented yajl-py version of json_verify.

--- examples/README.rst
+++ examples/README.rst
@@ -8,6 +8,10 @@ the example can also be run from ../ as::
 
 In this directory you will also find a python implementation of
 json_reformat, named json_reformat.py, it takes the same parameters as
-the C version, run it as follows for a thorough usage statement:
+the C version, run it as follows for a thorough usage statement::
 
     python json_reformat.py -h
+
+json_verify has also been implemented, to use run::
+
+    python json_verify.py -h
---
+++ examples/json_verify.py
@@ -0,0 +1,43 @@
+'''
+Python implementation of the Yajl C json_verify application
+'''
+
+import os
+import sys
+BASEPATH = os.path.dirname(os.path.realpath(__file__))
+sys.path = [BASEPATH, '%s/..' %BASEPATH] + sys.path
+from yajl import __version__ as yajl_version
+from yajl import *
+
+import optparse
+
+def main():
+    opt_parser = optparse.OptionParser(
+        description='validate json from stdin',
+        version='Yajl-Py for Yajl %s' %yajl_version)
+    opt_parser.add_option("-q",
+        action="store_false", dest="verbose", default=True,
+        help="quiet mode")
+    opt_parser.add_option("-c",
+        dest="allow_comments", action="store_true", default=False,
+        help="allow comments")
+    opt_parser.add_option("-u",
+        dest="check_utf8", action='store_false', default=True,
+        help="allow invalid utf8 inside strings")
+    (options, args) = opt_parser.parse_args()
+    yajl_parser = YajlParser(
+        allow_comments=options.allow_comments,
+        check_utf8=options.check_utf8)
+    retval = 0
+    try:
+        yajl_parser.parse()
+    except YajlError, e:
+        retval = 1
+        if options.verbose:
+            sys.stderr.write(str(e))
+    if options.verbose:
+        print "JSON is %s" %("invalid" if retval else "valid")
+    raise SystemExit(retval)
+
+if __name__ == "__main__":
+    main()

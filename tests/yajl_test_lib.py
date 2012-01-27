0ecf080bb0b3d55ef2f6e68232d88e0666106372
Hatem Nassrat <hnassrat@gmail.com>
2012-01-27 12:44:03 -0400

    Updating to use minimocktest library.

--- tests/yajl_test_lib.py
+++ tests/yajl_test_lib.py
@@ -5,28 +5,5 @@ sys.path = [BASEPATH, '%s/..' %BASEPATH] + sys.path
 
 import yajl
 import unittest
-import minimock
+from minimocktest import MockTestCase
 from StringIO import StringIO
-
-class MockTestCase(unittest.TestCase):
-    '''
-    A TestCase class that integrates minimock functionailty
-    `self.tt` minimock tracker object
-    `self.mock` calls minimock.mock using tracker=`self.tt`
-    `self.assertSameTrace` calls minimock.assert_same_trace with `self.tt`
-    '''
-    def setUp(self):
-        unittest.TestCase.setUp(self)
-        self.tt = minimock.TraceTracker()
-
-    def tearDown(self):
-        minimock.restore()
-        unittest.TestCase.tearDown(self)
-
-    def mock(self, *args, **kwargs):
-        if 'tracker' not in kwargs:
-            kwargs['tracker'] = self.tt
-        return minimock.mock(*args, **kwargs)
-
-    def assertSameTrace(self, want):
-        minimock.assert_same_trace(self.tt, want)
